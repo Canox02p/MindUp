10100cbd782c2dfac4b319f220d8b5ee3227e3fe
Canox02p <urielcano1243@gmail.com>
2025-10-28 22:32:17 -0600

    Initial Android project

--- app/src/main/java/com/example/mindup/MainActivity.kt
+++ app/src/main/java/com/example/mindup/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.tween
-import androidx.compose.runtime.*
-import com.example.mindup.ui.screen.main.MainScreen
-import com.example.mindup.ui.screen.welcome.SplashVideoScreen
+import com.example.mindup.ui.screen.container.ScreenContainer
 import com.example.mindup.ui.theme.MindUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,18 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindUpTheme {
-                var showSplash by remember { mutableStateOf(true) }
-
-                Crossfade(
-                    targetState = showSplash,
-                    animationSpec = tween(durationMillis = 450) // fade entre screens
-                ) { isSplash ->
-                    if (isSplash) {
-                        SplashVideoScreen(onFinished = { showSplash = false })
-                    } else {
-                        MainScreen()
-                    }
-                }
+                // 👇 Usa el NavHost (NavGraph) para manejar TODO el flujo
+                ScreenContainer()
             }
         }
     }
